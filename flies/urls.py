9cd306591d03286640aadaba39bccd7fc21bc4cf
kevin <josephkevin04@gmail.com>
2021-10-14 09:03:21 +0530

    requirement

--- flies/urls.py
+++ flies/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls')),
-    path('api/',include('core.api.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
